523a381e488479cb5d55601a45cbcf8be6be279d
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-18 12:16:04 -0300

    Typando los Props del Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
-import { useCart } from "../hooks/useCart"
+import type {CartItem, Guitar} from '../types'
 
-const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal}) => {
+type HeaderProps = {
+    cart: CartItem[]
+    removeFromCart: (id: Guitar['id']) => void
+    increaseQuantity: (id: Guitar['id']) => void
+    decreaseQuantity: (id: Guitar['id']) => void
+    clearCart: () => void
+    isEmpty: boolean
+    cartTotal: number
+}
+
+const Header = ({cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal} : HeaderProps) => {
 
   return (
     <header className="py-5 header">
