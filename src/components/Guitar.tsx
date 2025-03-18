cfffb9eeb6a0a370dac5f3866aa3a55101ccbfff
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-18 10:59:56 -0300

    Type Separado

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -6,7 +6,12 @@ type Guitar = {
   price: number
 }
 
-const Guitar = ({guitar, addToCart} : {guitar: Guitar, addToCart: (item: Guitar) => void }) => {
+type GuitarProps = {
+  guitar: Guitar
+  addToCart: (item: Guitar) => void 
+}
+
+const Guitar = ({guitar, addToCart} : GuitarProps) => {
 
   const {name, image, description, price} = guitar;
 
